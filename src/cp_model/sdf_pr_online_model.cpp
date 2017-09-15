d03acdd3586cb45a867c56a57dfdefb21924ae8a
dannmann <krosvall@kth.se>
2017-09-15 08:25:16 +0200

    checking something for experiment 1 (exp_19)

--- src/cp_model/sdf_pr_online_model.cpp
+++ src/cp_model/sdf_pr_online_model.cpp
@@ -63,6 +63,8 @@ SDFPROnlineModel::SDFPROnlineModel(Mapping* p_mapping, Config* _cfg):
     IntVarArgs wcct_b(*this, apps->n_programChannels(), 0, Int::Limits::max); 
     IntVarArgs wcct_s(*this, apps->n_programChannels(), 0, Int::Limits::max); 
     IntVarArgs wcct_r(*this, apps->n_programChannels(), 0, Int::Limits::max); 
+    
+    LOG_DEBUG("Int::Limits::max = " + tools::toString(Int::Limits::max));
 
     std::ostream debug_stream(nullptr); /**< debuging stream, it is printed only in debug mode. */
     debug_stream << "\n==========\ndebug log:\n..........\n";
