575292a0c24eecadf40f318da03b50ca8d3c1670
dannmann <krosvall@kth.se>
2017-09-26 08:41:56 +0200

    fixed config files for experiments

--- examples/TODAES/fix_experiments.sh
+++ examples/TODAES/fix_experiments.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 
-for i in $( ls -d DATE_*5); do
+for i in $( ls -d exp_*); do
     echo dir: $i
+    DIRNAME=$(echo $i| cut -d'/' -f 1)
+    rm $i/procData.csv
+    #cp exp_3_1_rajp/config.cfg $DIRNAME/config.cfg
+    #sed -i -e "s:exp_3_1_rajp/:$DIRNAME/:g" $DIRNAME/config.cfg
+    #sed -i -e 's:criteria=THROUGHPUT:criteria=THROUGHPUT'"\n"'criteria=POWER:g' $DIRNAME/config.cfg
     #rm -rf $i/out/
     #rm $i/desConst.csv
     #rm $i/wcet.csv
     #mkdir -p $i/xmls
-    cp ./platform.xml $i/xmls/platform.xml
+    #cp ./platform.xml $i/xmls/platform.xml
     #cp ./desConst.xml $i/xmls/desConst.xml
     #move all sdf graph files into an sdfs directory
     #for j in $( ls $i/*.*sdf.xml); do
